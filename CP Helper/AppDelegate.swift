2c3e4a6e12489d9a6727b61c89424d526dc641bc
n0sc0p3 <58154057+n0sc0p3@users.noreply.github.com>
2021-07-11 02:34:03 +0530

    Initial Commit

---
+++ CP Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CP Helper
+//
+//  Created by Lithium on 11/07/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CP Helper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CP Helper
+//
+//  Created by Lithium on 11/07/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
